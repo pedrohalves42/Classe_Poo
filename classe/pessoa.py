f781d91af2dc33fc65dc6a54b486f9b2c444209d
pedrohalves42@gmail.com <pedrohalves42@gmail.com>
2022-06-29 09:21:09 -0300

    classe

--- classe/pessoa.py
+++ classe/pessoa.py
@@ -1,4 +1,9 @@
+from datetime import  datetime
+
+
 class Pessoa:
+    ano_atual = int(datetime.strftime(datetime.now(), '%Y'))
+
     def __init__(self, nome, idade, comendo=False, falando=False):
         self.nome = nome
         self.idade = idade
@@ -45,3 +50,6 @@ class Pessoa:
 
         print(f'{self.nome} parou de comer.')
         self.comendo = False
+
+    def get_ano_nascimento(self):
+        return self.ano_atual - self.idade
